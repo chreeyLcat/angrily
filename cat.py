ab0fbbb9d69b2496038f64102fa1b560d4b3acc3
chreey <942026195@qq.com>
2017-11-28 19:27:26 +0800

    更改

--- cat.py
+++ cat.py
@@ -32,7 +32,7 @@ for i in range(4):
         else:
             print("账户不存在！")
     else:
-        print("错误操作超过三次，请联系管理员")
+        print("错误操作超过三次，请联系管理员!!!")
         exit()
 user = {"name":username,"age":"18岁","high":"168cm","job":"测试工程师","sex":"女"}
 for key in user:
